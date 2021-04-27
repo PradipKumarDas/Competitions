763c83dcab0f24a5d92d5a3527ea0e67b8946bd2
Pradip Kumar Das <pradipkumardas@hotmail.com>
2021-04-27 11:47:52 +0530

    Added Dense Fully Connected Neural Network model, main and predictor python files, input CSV files and requirements.txt file that indicates package availability in contest environment

---
+++ IPL_2021_Cricket_Hackathon_IIT_Madras/predictor.py
@@ -0,0 +1,127 @@
+# Imports required packages
+
+import os
+import warnings
+
+import numpy as np
+import pandas as pd
+
+from tensorflow.keras import models
+
+import pickle
+
+
+# Accepts path to input file, performs prediction and returns the value
+def predictRuns(testInput):
+
+    # Disables Tensorflow's information and warnings to be printed in console
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    
+    # Disables python's warnings to be printed in console
+    warnings.filterwarnings("ignore")
+    
+    prediction = 0
+    
+    # Updates these venues that are mentioned in different names with same name
+
+    input_data = pd.read_csv(testInput)
+    
+    input_data.venue[input_data.venue.str.contains("Brabourne", 
+                                                 case=False)] = "Brabourne Stadium"
+    
+    input_data.venue[input_data.venue.str.contains("Chinnaswamy", 
+                                                 case=False)] = "M. Chinnaswamy Stadium"
+    
+    input_data.venue[input_data.venue.str.contains("Chidambaram", 
+                                                 case=False)] = "M. A. Chidambaram Stadium"
+    
+    input_data.venue[input_data.venue.str.contains(r'Narendra Modi', 
+                                                 case=False)] = "Narendra Modi Stadium"
+
+    input_data.venue[input_data.venue.str.contains(r'Punjab Cricket|IS Bindra|Inderjit Singh Bindra', 
+                                                 case=False)] = "IS Bindra Stadium"
+    
+    input_data.venue[input_data.venue.str.contains("Rajiv Gandhi", 
+                                                 case=False)] = "Rajiv Gandhi International Cricket Stadium"
+    
+    input_data.venue[input_data.venue.str.contains("Wankhede", 
+                                                 case=False)] = "Wankhede Stadium"
+
+    
+    # Loads venue encoder and encodes venue
+    
+    with open("Models/Venue_Encoder.pickle", "rb") as f:
+        venue_encoder = pickle.load(f)
+    venue_encoded = pd.DataFrame(venue_encoder.transform(
+        input_data[["venue"]]).toarray(), columns=[("venue_" + str(i)) for i in range(len(venue_encoder.get_feature_names()))])
+    input_data = input_data.join(venue_encoded).drop(["venue"], axis=1)
+
+    # Loads innings encoder and encodes innings
+    
+    with open("Models/Innings_Encoder.pickle", "rb") as f:
+        innings_encoder = pickle.load(f)
+    innings_encoded = pd.DataFrame(innings_encoder.transform(
+        input_data[["innings"]]).toarray(), columns=[("innings_" + str(i)) for i in range(len(innings_encoder.get_feature_names()))])
+    input_data = input_data.join(innings_encoded).drop(["innings"], axis=1)
+
+
+    # Loads teams encoder and encodes batting and bowling team
+    
+    with open("Models/Team_Encoder.pickle", "rb") as f:
+        team_encoder = pickle.load(f)
+
+    batting_team_encoded = pd.DataFrame(team_encoder.transform(
+        input_data[["batting_team"]]).toarray(), columns=[("batting_team_" + str(i)) for i in range(len(team_encoder.get_feature_names()))])
+    input_data = input_data.join(batting_team_encoded).drop(["batting_team"], axis=1)
+    
+    bowling_team_encoded = pd.DataFrame(team_encoder.transform(
+        input_data[["bowling_team"]]).toarray(), columns=[("bowling_team_" + str(i)) for i in range(len(team_encoder.get_feature_names()))])
+    input_data = input_data.join(bowling_team_encoded).drop(["bowling_team"], axis=1)
+
+
+    # Loads player encoder and encodes both batsmen and bowlers
+    
+    with open("Models/Player_Encoder.pickle", "rb") as f:
+        player_encoder = pickle.load(f)
+
+    batsmen = pd.DataFrame(input_data.iloc[0]["batsmen"].split(','), columns=["batsmen"])
+    
+    batsmen_encoded_array = player_encoder.transform(batsmen[["batsmen"]]).toarray()
+    batsmen_encoded_array = batsmen_encoded_array.sum(axis=0)
+    batsmen_encoded_array = batsmen_encoded_array.reshape(1, len(batsmen_encoded_array))
+    
+    batsmen_encoded = pd.DataFrame(batsmen_encoded_array,
+            columns=[("batsman_" + str(i)) for i in range(len(player_encoder.get_feature_names()))])
+    input_data = input_data.join(batsmen_encoded).drop(["batsmen"], axis=1)
+    
+    
+    bowlers = pd.DataFrame(input_data.iloc[0]["bowlers"].split(','), columns=["bowlers"])
+    
+    bowlers_encoded_array = player_encoder.transform(bowlers[["bowlers"]]).toarray()
+    bowlers_encoded_array = bowlers_encoded_array.sum(axis=0)
+    bowlers_encoded_array = bowlers_encoded_array.reshape(1, len(bowlers_encoded_array))
+    
+    bowlers_encoded = pd.DataFrame(bowlers_encoded_array,
+            columns=[("bowler_" + str(i)) for i in range(len(player_encoder.get_feature_names()))])
+    input_data = input_data.join(bowlers_encoded).drop(["bowlers"], axis=1)
+    
+    
+    # Convers DataFrame into Numpy array tp feed into neural network model
+    
+    input_data_array = input_data.to_numpy()    
+    
+
+    # Loads model
+    model = models.load_model("Models/Dense_ANN_Regressor")
+    
+    # Predicts
+    
+    prediction = model.predict(input_data_array)
+    
+    # Rounds off predicted number to its nearest integer
+    
+    prediction = round(prediction[0][0])
+        
+    # Returns the final prediction to the caller
+    
+    return prediction
